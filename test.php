c438937c4b7646cfcaddc28aa61d64c864e46fed
royfee <royfee@126.com>
2022-04-28 15:31:53 +0800

    test

--- test.php
+++ test.php
@@ -7,11 +7,11 @@
     $res = $track->config([
         /*
         'trackingmore' => [
-            'apikey' => 'b3ab41cd-9d60-4cc2-991e-dc1763abe6f1'//c336bfbc-2c46-4e14-ba21-aee8669d843e
+            'apikey' => '
         ]
        */
         'track17'   =>  [
-            'token' =>  '44F4F4002BA24A8FFC9A18033AC3A085'
+            'token' =>  ''
         ]
         
     ])->tracking('1186545870767');//,['original'=>true]EK485132442HKEK485132456HK
